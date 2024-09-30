8aef6dc7b9d20320fa265a26d3d17d88ecc29345
M.VINOTHKUMAR <166404677+M-VINOTHKUMAR@users.noreply.github.com>
2024-09-30 11:39:50 +0530

    Time: 1 ms (54.33%), Space: 41.3 MB (80.9%) - LeetHub

--- 1859-sorting-the-sentence/1859-sorting-the-sentence.java
+++ 1859-sorting-the-sentence/1859-sorting-the-sentence.java
@@ -2,28 +2,27 @@ class Solution {
     public String sortSentence(String s) {
         String a[]=s.split(" ");
         int n=a.length;
-        for(int i=0;i<n;i++)
+        for(int i=0;i<n-1;i++)
         {
-           
-            for(int j=0;j<n;j++)
+            for(int j=0;j<n-i-1;j++)
             {
-                 char c1=a[i].charAt(a[i].length()-1);
+                 char c1=a[j+1].charAt(a[j+1].length()-1);
                   char c2=a[j].charAt(a[j].length()-1);
                if(c1<c2)
                {
-                 String t=a[i];
-                 a[i]=a[j];
-                 a[j]=t;
+                 String t=a[j];
+                 a[j]=a[j+1];
+                 a[j+1]=t;
                }
            //  System.out.println(c1+" "+c2);
             }
            // System.out.println(Arrays.toString(a));
         }
-            s="";
+           StringBuilder sb=new StringBuilder();
             for(int i=0;i<a.length;i++)
             {
-                s+=a[i].substring(0,a[i].length()-1)+" ";
+                sb.append(a[i].substring(0,a[i].length()-1)+" ");
             }
-            return s.trim();
+            return sb.toString().trim();
     }
 }
