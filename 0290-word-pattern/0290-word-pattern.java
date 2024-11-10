94e43aac5c271f16716dcdefd45106e4686bc10f
M.VINOTHKUMAR <166404677+M-VINOTHKUMAR@users.noreply.github.com>
2024-11-10 12:31:25 +0530

    Time: 1 ms (81.45%), Space: 41.3 MB (66.86%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.java
+++ 0290-word-pattern/0290-word-pattern.java
@@ -1,28 +1,26 @@
 class Solution {
     public boolean wordPattern(String pattern, String s) {
-        String a[]=s.split(" ");
-      //  System.out.print(a.length);
-        int n=pattern.length();
-       if(pattern.length()!=a.length) return false;
-        HashMap<Character,String> m=new   HashMap<>();
-        m.put(pattern.charAt(0),a[0]);
-        for(int i=1;i<n;i++)
+        String a[]=new String[27];
+        String p[]=new String[27];
+        String b[]=s.split(" ");
+        int n1=pattern.length();
+        int n2=b.length;
+        if(n1!=n2)
+           return false;
+         Arrays.fill(a,"");
+        for(int i=0;i<n1;i++)
         {
-            if(!m.containsKey(pattern.charAt(i)) && !m.containsValue(a[i]))
+            int po=pattern.charAt(i)-97;
+            if(!a[po].equals("") && !a[po].equals(b[i]) )
+              return false;
+            a[po]=b[i];
+            for(int j=0;j<26;j++)
             {
-                  m.put(pattern.charAt(i),a[i]);
+                if(p[j]!=null && p[j].equals(b[i]) && j!=po)
+                  return false;
             }
-            else
-            {
-                String c=m.get(pattern.charAt(i));
-               //  System.out.print(m);
-                if(!a[i].equals(c))
-                {
-                          return false;
-                }
-            }
-        }
+            p[po]=b[i];
+        }   
         return true;
-        
     }
 }
