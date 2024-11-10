4461310f71093377037f6f9e09604b9b98d0ae31
M.VINOTHKUMAR <166404677+M-VINOTHKUMAR@users.noreply.github.com>
2024-11-10 09:58:05 +0530

    Time: 1 ms (89.49%), Space: 42 MB (29.67%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.java
+++ 0097-interleaving-string/0097-interleaving-string.java
@@ -1,23 +1,15 @@
 class Solution {
-    public boolean rec(String s1,String s2,String s3,int i,int j, int k)
+    public boolean rec(String s1,String s2,String s3,int i,int j, int k,Boolean dp[][])
     {
         if(k==-1 && i==-1 && j==-1)
            return true;
         if(k==-1)
             return false;
-        
-         if(i!=-1 && s1.charAt(i)==s3.charAt(k))
-        {
-           if( rec(s1,s2,s3,i-1,j,k-1))
-            return true;
-        }
-        if(j!=-1 && s2.charAt(j)==s3.charAt(k))
-        {
-            if(rec(s1,s2,s3,i,j-1,k-1))
-             return true;
-        }
-      
-              return false;
+        if(dp[i+1][j+1]  !=(null))
+            return dp[i+1][j+1];
+            boolean a=i!=-1 && s1.charAt(i)==s3.charAt(k) &&rec(s1,s2,s3,i-1,j,k-1,dp);
+            boolean b=j!=-1 && s2.charAt(j)==s3.charAt(k) &&rec(s1,s2,s3,i,j-1,k-1,dp);
+          return dp[i+1][j+1]=a||b;
       
     }
     public boolean isInterleave(String s1, String s2, String s3) {
@@ -25,6 +17,7 @@ class Solution {
        int l1=s1.length(),l2=s2.length(),l3=s3.length();
        if(l1+l2!=l3)
          return false;
-      return rec(s1,s2,s3,l1-1,l2-1,l3-1);
+        Boolean dp[][]=new Boolean[l1+1][l2+1];
+      return rec(s1,s2,s3,l1-1,l2-1,l3-1,dp);
     }
 }
