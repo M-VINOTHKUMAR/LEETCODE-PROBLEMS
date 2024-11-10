f15c3bfd0b28b611493093dd07e22d98d4724f9b
M.VINOTHKUMAR <166404677+M-VINOTHKUMAR@users.noreply.github.com>
2024-11-10 11:47:33 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,18 +1,43 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int l1=nums1.length;
-        int l2=nums2.length;
-        int a[]=new int[l1+l2];
-        for(int i=0;i<l1;i++)
-          a[i]=nums1[i];
-        for(int i=0;i<l2;i++)
-          a[l1+i]=nums2[i];
-          Arrays.sort(a);
-        if((l1+l2)%2==1)
-            return (double)a[(l1+l2-1)/2];
-        else{
-            int mid=(l1+l2)/2;
-            return (double) (a[mid]+a[mid-1])/2;
-        }
+        int l1=nums1.length+nums2.length;
+         int ind1=l1/2;
+         int ind2=l1/2+1;
+         int i=0,j=0,c=0;
+         int v1=0,v2=0;
+         while(i<nums1.length && j<nums2.length)
+         {
+            if(nums1[i]<nums2[j])
+            {
+                  if(c==ind1) v1=nums1[i];
+                  if(c==ind2) v2=nums1[i];
+                  c++;
+                  i++;
+            }
+            else{
+                if(c==ind1) v1=nums2[j];
+                  if(c==ind2) v2=nums2[j];
+                  c++;
+                  j++;
+            }
+         }
+            while(i<nums1.length)
+         {
+                  if(c==ind1) v1=nums1[i];
+                  if(c==ind2) v2=nums1[i];
+                  c++;
+                  i++;
+         }
+             while(j<nums2.length)
+         {
+                  if(c==ind1) v1=nums1[j];
+                  if(c==ind2) v2=nums1[j];
+                  c++;
+                  j++;
+         }
+         if(l1%2==1)
+            return (double) v1;
+            System.out.println(v1+" "+v2);
+          return ((double)v1+(double)v2)/2;
 }
 }
