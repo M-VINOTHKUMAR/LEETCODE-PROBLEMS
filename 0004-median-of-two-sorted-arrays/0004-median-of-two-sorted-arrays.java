17ab42163dc951556c2107627ac0ece1ad5b8c57
M.VINOTHKUMAR <166404677+M-VINOTHKUMAR@users.noreply.github.com>
2024-12-13 19:24:23 +0530

    Time: 2 ms (59.12%), Space: 45.7 MB (85.64%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,43 +1,24 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int l1=nums1.length+nums2.length;
-         int ind1=l1/2;
-         int ind2=l1/2-1;
-         int i=0,j=0,c=0;
-         int v1=0,v2=0;
-         while(i<nums1.length && j<nums2.length)
-         {
+        int n1=nums1.length;
+        int n2=nums2.length;
+        int res[]=new int[n1+n2];
+        int i=0,j=0,k=0;
+        while(i<n1 && j<n2)
+        {
             if(nums1[i]<nums2[j])
-            {
-                  if(c==ind1) v1=nums1[i];
-                  if(c==ind2) v2=nums1[i];
-                  c++;
-                  i++;
-            }
-            else{
-                if(c==ind1) v1=nums2[j];
-                  if(c==ind2) v2=nums2[j];
-                  c++;
-                  j++;
-            }
-         }
-            while(i<nums1.length)
-         {
-                  if(c==ind1) v1=nums1[i];
-                  if(c==ind2) v2=nums1[i];
-                  c++;
-                  i++;
-         }
-             while(j<nums2.length)
-         {
-                  if(c==ind1) v1=nums2[j];
-                  if(c==ind2) v2=nums2[j];
-                  c++;
-                  j++;
-         }
-         if(l1%2==1)
-            return (double) v1;
-           // System.out.println(v1+" "+v2);
-          return ((double)v1+(double)v2)/2;
-}
+               res[k++]=nums1[i++];
+            else
+               res[k++]=nums2[j++];
+        }
+        while(i<n1)
+          res[k++]=nums1[i++];
+        while(j<n2)
+          res[k++]=nums2[j++];
+          int mid=(n1+n2)/2;
+        if((n1+n2)%2==0)
+           return (double)(res[mid-1]+res[mid])/2;
+        else
+           return res[mid];
+    }
 }
