d8de2f819364934d1219503c3ec72c31a0e102c8
M.VINOTHKUMAR <166404677+M-VINOTHKUMAR@users.noreply.github.com>
2024-12-29 13:59:03 +0530

    Time: 1674 ms (6.59%), Space: 0B (100%) - LeetHub

--- 1327-list-the-products-ordered-in-a-period/1327-list-the-products-ordered-in-a-period.sql
+++ 1327-list-the-products-ordered-in-a-period/1327-list-the-products-ordered-in-a-period.sql
@@ -6,7 +6,7 @@ SELECT *
 from Products
 join Orders
 using(product_id)
-where order_date between '2020-02-01' and '2020-02-28'
+where order_date between '2020-02-01' and '2020-02-29'
 ) as t
 group by product_id
 having unit>99;
