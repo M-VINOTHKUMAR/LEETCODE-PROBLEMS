6a4a473eb9e56e1701779a5bd587bef97499513f
M.VINOTHKUMAR <166404677+M-VINOTHKUMAR@users.noreply.github.com>
2025-01-08 23:01:24 +0530

    Time: 1 ms (70.67%), Space: 41.6 MB (48.48%) - LeetHub

--- 0394-decode-string/0394-decode-string.java
+++ 0394-decode-string/0394-decode-string.java
@@ -2,43 +2,49 @@ class Solution {
     public String decodeString(String s) {
         Stack<String > str=new Stack<>();
         Stack<Integer> d=new Stack<>();
-       
         for(int i=0;i<s.length();i++)
         {
             char c=s.charAt(i);
-            StringBuilder res=new StringBuilder("");
+            // if "[" occurs then 
             if(c==']')
             {
-            while(!str.peek().equals("["))
-            {
-                res.insert(0,str.pop());
-            }
-            int key=d.pop();
-             String temp="";
-             for(int j=0;j<key;j++)
-             {
-               temp+=(res.toString());
-             }
-             str.pop();
-             str.push(temp);
+                // string is collected
+                 StringBuilder res=new StringBuilder("");
+                  while(!str.peek().equals("["))
+                  {
+                 res.insert(0,str.pop());
+                 }
+                  int key=d.pop();
+                  String temp="";  
+                  // repeat the string 
+                for(int j=0;j<key;j++)
+                {
+                    temp+=(res.toString());
+                }
+             str.pop(); // pop the "["
+             str.push(temp); // push the repeated  string 
             }
            else  if(Character.isDigit(c))
             {
+                // create the digit 
                 int repdig=0;
-                while(Character.isDigit(s.charAt(i)))
+                while(Character.isDigit(s.charAt(i))) // more than one digit
                 {
                      repdig=repdig*10+(s.charAt(i)-48);
                      i++;
                 }
                 d.push(repdig);
                 i--;
-            }else
-        {
+            }
+            else
+             {
+                // push the string into the string stack
             str.push(String.valueOf(c));
-        }
-      //  System.out.println(str+" "+d);
+        
+            }
         }
          StringBuilder res=new StringBuilder("");
+         // collect the final string 
         while(!str.isEmpty())
         {
             if(!str.peek().equals("["))
